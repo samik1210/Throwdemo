1d0a539cc17640bc3091c4b78cfd0c60efe1f50d
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:19:18 +0530

    Add project files.

---
+++ Throwdemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Throwdemo
+{
+    class Student
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+
+
+                static void Method1()
+                {
+                    try
+                    {
+                        Method2();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+
+                static void Method2()
+                {
+                    string str = null;
+                    try
+                    {
+                        Console.WriteLine(str[0]);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
